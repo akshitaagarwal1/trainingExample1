9b537f8aa8df198efcea4b69957d317d475cc4c6
akshitaagarwal1 <akshitaagarwal.0723@gmail.com>
2015-09-30 12:42:02 +0530

    yoo!!

---
+++ Threads/src/com/training/JoinExample.java
@@ -0,0 +1,18 @@
+package com.training;
+
+public class JoinExample {
+	public static void main(String[] args){
+		Thread t=new Thread(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				super.run();
+			} 
+			
+		};
+		
+		
+		
+	}
+}
